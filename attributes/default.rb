245482fcf7dc8a2cba9b0df790448c5bdcbac37c
Richard Shade <rshade@rightscale.com>
2016-07-20 15:23:54 -0500

    updating log files var

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,8 @@
 default['cwlogs']['region'] = 'us-east-1'
 default['cwlogs']['state_file_dir'] = '/var/awslogs/state'
 default['cwlogs']['state_file_name'] = 'agent-state'
+default['cwlogs']['logfiles'] = [
+  '/var/log/messages',
+  '/var/log/httpd/access_log',
+  '/var/log/httpd/error_log'
+]
