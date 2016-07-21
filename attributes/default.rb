04fc939a0b2dbf39f3a7d15983c74500e69e2f7d
Richard Shade <rshade@rightscale.com>
2016-07-21 13:49:06 -0500

    fixing name

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 default['cwlogs']['region'] = 'us-east-1'
 default['cwlogs']['state_file_dir'] = '/var/awslogs/state'
 default['cwlogs']['state_file_name'] = 'agent-state'
-default['cwlogs']['logfiles']["#{node['cwlogs']['deployment']}-messages"] = {
+default['cwlogs']['logfiles']["#{node['cwlogs']['deployment'].gsub(' ','-').gsub('+','-').squeeze}-messages"] = {
     'datetime_format' => '[%Y-%m-%d %H:%M:%S]',
-    'log_group_name' => node['cwlogs']['deployment'],
+    'log_group_name' => node['cwlogs']['deployment'].gsub(' ','-').gsub('+','-').squeeze
     'file' => '/var/log/messages',
     'log_stream_name' => "#{node['cwlogs']['hostname']}-var-log-messages"
 }
