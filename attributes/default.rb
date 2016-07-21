9ff58a8e7fcb7d483c50620a68b523d87082ac4b
Richard Shade <rshade@rightscale.com>
2016-07-21 15:11:25 -0500

    update httpd

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,25 @@
 default['cwlogs']['region'] = 'us-east-1'
 default['cwlogs']['state_file_dir'] = '/var/awslogs/state'
 default['cwlogs']['state_file_name'] = 'agent-state'
+#v-l-m
 default['cwlogs']['logfiles']["#{node['cwlogs']['deployment'].gsub(' ','-').gsub('+','-').squeeze}-messages"] = {
     'datetime_format' => '[%Y-%m-%d %H:%M:%S]',
     'log_group_name' => "#{node['cwlogs']['deployment'].gsub(' ','-').gsub('+','-').squeeze}-messages",
     'file' => '/var/log/messages',
-    'log_stream_name' => "#{node['cwlogs']['hostname']}-var-log-messages"
+    'log_stream_name' => "#{node['cwlogs']['hostname']}"
+}
+
+#error_log
+default['cwlogs']['logfiles']["#{node['cwlogs']['deployment'].gsub(' ','-').gsub('+','-').squeeze}-httpd_error_log"] = {
+    'datetime_format' => '[%Y-%m-%d %H:%M:%S]',
+    'log_group_name' => "#{node['cwlogs']['deployment'].gsub(' ','-').gsub('+','-').squeeze}-httpd_error_log",
+    'file' => '/var/log/httpd/error_log',
+    'log_stream_name' => "#{node['cwlogs']['hostname']}"
+}
+#access_log
+default['cwlogs']['logfiles']["#{node['cwlogs']['deployment'].gsub(' ','-').gsub('+','-').squeeze}-httpd_access_log"] = {
+    'datetime_format' => '[%Y-%m-%d %H:%M:%S]',
+    'log_group_name' => "#{node['cwlogs']['deployment'].gsub(' ','-').gsub('+','-').squeeze}-httpd_access_log",
+    'file' => '/var/log/httpd/access_log',
+    'log_stream_name' => "#{node['cwlogs']['hostname']}"
 }
