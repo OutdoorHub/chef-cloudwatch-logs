672adfa777b5e997f2d67f69b74655f150e3ebbe
Richard Shade <rshade@rightscale.com>
2016-07-21 14:42:34 -0500

    fixing hash

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default['cwlogs']['state_file_dir'] = '/var/awslogs/state'
 default['cwlogs']['state_file_name'] = 'agent-state'
 default['cwlogs']['logfiles']["#{node['cwlogs']['deployment'].gsub(' ','-').gsub('+','-').squeeze}-messages"] = {
     'datetime_format' => '[%Y-%m-%d %H:%M:%S]',
-    'log_group_name' => node['cwlogs']['deployment'].gsub(' ','-').gsub('+','-').squeeze
+    'log_group_name' => "#{node['cwlogs']['deployment'].gsub(' ','-').gsub('+','-').squeeze}",
     'file' => '/var/log/messages',
     'log_stream_name' => "#{node['cwlogs']['hostname']}-var-log-messages"
 }
